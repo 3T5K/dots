55babb2d8336f0f033b5b5876e1882719ced8909
3T5K <ivank.dbt@gmail.com>
2024-03-25 22:27:27 +0100

    fixed a hang; added more logs

--- zsh-conf/install.sh
+++ zsh-conf/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO: implement cleanup/operation revert on failure
+
 log() {
 	printf "\033[32m%s\033[0m\n" "$1"
 }
@@ -12,13 +14,13 @@ die() {
 backup() {
 	log "looking for '$1'"
 	[ -e "$HOME/$1" ] &&\
-		(mv -v "$HOME/$1" "$HOME/$1.bak"\
+		(mv -v "$HOME/$1" "$HOME/$1.bak-$(uuidgen))"\
 			|| die "failed to back up '~/$1'... quitting"\
 			&& echo "backed up")
 }
 
 log "pinging github"
-ping github.com 1>/dev/null 2>&1 || die "failed to reach github.com (make sure there is a network connection)"
+ping -c 2 github.com 1>/dev/null 2>&1 || die "failed to reach github.com (make sure there is a network connection)"
 echo "ok"
 
 log "looking for dependencies"
@@ -36,12 +38,15 @@ log "downloading powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 log "installing config"
-cp -v zshenv "$HOME/.zshenv" || printf "failed to copy zshenv -> ~/.zshenv" 1>&2
-cp -rv zsh "$HOME/.config/"  || printf "failed to copy zsh -> ~/.config/zsh" 1>&2
-
+cp -v zshenv "$HOME/.zshenv" || die "failed to copy zshenv -> ~/.zshenv"
+cp -rv zsh "$HOME/.config/"  || die "failed to copy zsh -> ~/.config/zsh"
 
 log "downloading zsh plugins"
-git clone https://github.com/MichaelAquilina/zsh-auto-notify.git "$HOME/.config/zsh/plugins/zsh-auto-notify"
-git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.config/zsh/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-history-substring-search.git "$HOME/.config/zsh/plugins/zsh-history-substring-search"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.config/zsh/plugins/zsh-syntax-highlighting"
+log "[ auto notify ]"
+git clone https://github.com/MichaelAquilina/zsh-auto-notify.git "$HOME/.config/zsh/plugins/zsh-auto-notify" || die failed...
+log "[ auto suggestions ]"
+git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.config/zsh/plugins/zsh-autosuggestions" || die failed...
+log "[ history substring search ]"
+git clone https://github.com/zsh-users/zsh-history-substring-search.git "$HOME/.config/zsh/plugins/zsh-history-substring-search" || die failed...
+log "[ syntax highlighting ]"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.config/zsh/plugins/zsh-syntax-highlighting" || die failed...
